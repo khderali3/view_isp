2612fa619f737e1e63f6e99124a61a257c840be8
khderali3 <khdersliman3@gmail.com>
2025-07-10 15:56:11 +0300

    updted on 20250710 - 15:56

--- back/django_project/logSystemApp/apps.py
+++ back/django_project/logSystemApp/apps.py
@@ -1,9 +1,21 @@
+# from django.apps import AppConfig
+
+
+# class LogsystemappConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'logSystemApp'
+
+#     def ready(self):
+#         import logSystemApp.signals
+
+
 from django.apps import AppConfig
+import sys
 
-
-class LogsystemappConfig(AppConfig):
+class LogSystemAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'logSystemApp'
 
     def ready(self):
-        import logSystemApp.signals
+        if 'migrate' not in sys.argv:
+             import logSystemApp.signals
